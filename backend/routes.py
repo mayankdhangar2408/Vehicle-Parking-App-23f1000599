1fb03b16f08658e959f47ba4153568e77dd23c9a
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-07-05 22:41:22 +0530

    login and signup

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,20 @@
 from app import app
+from flask import render_template, request
 
 @app.route("/")
 def index():
-    return "hello World"
+    return render_template("home.html")
 
-
-@app.route("/home")
-def home():
-    return "welcome to home"
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    if request.method == "GET": 
+        return render_template("register.html")
+    elif request.method == "POST":
+        u_name = request.form.get("name")
+        u_email = request.form.get("email")
+        u_password = request.form.get("password")
+        u_phone = request.form.get("phone")
 
 @app.route("/login")
 def login():
-    return "welcome to login"
+    return render_template("login.html")
