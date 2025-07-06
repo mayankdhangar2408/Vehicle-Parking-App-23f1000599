90cc90b8d4df933540984517bf902e55cc86acaf
Mayank Dhangar <mayankdhangar78060@gmail.com>
2025-07-06 21:46:51 +0530

    Dashboard Setup

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
+from .models import db, Admin, User, ParkingLot
 
 @app.route("/")
 def index():
@@ -14,7 +15,40 @@ def register():
         u_email = request.form.get("email")
         u_password = request.form.get("password")
         u_phone = request.form.get("phone")
+        user = db.session.query(User).filter_by(email = u_email).first()
+        if user:
+            return "E-mail Already Exist"
+        else:
+            new_user = User(name = u_name, email = u_email, password = u_password, phone = u_phone)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/login")
 
-@app.route("/login")
+@app.route("/login", methods =["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        l_email = request.form.get("email")
+        l_password = request.form.get("password")
+
+        login = db.session.query(Admin).filter_by(email=l_email).first() or db.session.query(User).filter_by(email=l_email).first()
+
+        if login:
+            if login.password == l_password:
+                if isinstance(login, Admin):
+                    return redirect(f"/admin/dashboard?login_id={login.id}")
+                elif isinstance(login, User):
+                    return redirect(f"/user/dashboard?login_id={login.id}")
+            else:
+                return "Incorrect Password"
+        else:
+            return "E-mail doesn't exist"
+
+@app.route("/admin/dashboard")
+def admin_dash():
+    return render_template("/admin/dashboard.html")
+
+@app.route("/user/dashboard")
+def user_dash():
+    return render_template("/user/dashboard.html")
